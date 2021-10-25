b4148372273002c70a43be9e4f0e5aff114355f9
Tyler Petersen <tylerpetersen02@gmail.com>
2021-10-25 14:50:26 -0700

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, image, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    className={`${size} menu-item`}
+    className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className='background-image'
       style={{
-        backgroundImage: `url(${image})`
+        backgroundImage: `url(${imageUrl})`
       }}
     ></div>
     <div className='content'>
@@ -19,4 +20,4 @@ const MenuItem = ({ title, image, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
